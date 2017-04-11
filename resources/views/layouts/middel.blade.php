75bdaf3aa092744841b70886f7163f0e149872ae
Anton-Patokin <patokin.anton@gmail.com>
2017-04-11 20:01:07 +0200

    add active helper

--- resources/views/layouts/middel.blade.php
+++ resources/views/layouts/middel.blade.php
@@ -2,9 +2,9 @@
     <nav class="navbar navbar-default" role="navigation">
         <div class="">
             <ul class="nav navbar-nav">
-                <li><a class="active" href="{{url('/home')}}">Home</a></li>
-                <li><a href="{{url('/dashboard')}}">Dashboard</a></li>
-                <li {{isActiveRoute('toevoegen')}}><a href="{{url('/toevoegen')}}">Toevoegen</a></li>
+                <li><a class="{{Menu::isActiveRoute('home')}}" href="{{url('/home')}}">Home</a></li>
+                <li><a class="{{Menu::isActiveRoute('dashboard')}}" href="{{url('/dashboard')}}">Dashboard</a></li>
+                <li  ><a class="{{Menu::isActiveRoute('toevoegen')}}" href="{{url('/toevoegen')}}">Toevoegen</a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </nav>
