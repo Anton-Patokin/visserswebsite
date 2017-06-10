84a9b8bd229649fd99dcb776677e9e9383d1a0ec
Anton-Patokin <patokin.anton@gmail.com>
2017-06-10 19:04:42 +0200

    cors removed

--- routes/api.php
+++ routes/api.php
@@ -12,35 +12,29 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group([
-    'middleware' => ['api', 'cors'],
-    'prefix' => 'api',
-], function () {
 
-    Route::middleware('auth:api')->get('/user', function (Request $request) {
-        return $request->user();
-    });
-    Route::post('/add/content', 'AddContentController@add');
-    Route::get('/add/content', function () {
-        return 'okey';
-    });
-
-    Route::get('/get/markers/{type}', 'ApiGoogleMaps@markers');
-    Route::get('/get/marker/{id}', 'ApiGoogleMaps@marker');
-    Route::post('/zoeken/googleMaps', 'ApiZoekenController@googleMapsZoeken');
-    Route::post('/zoeken/googleMaps/visPlatsen', 'ApiZoekenController@googleMapsZoekenVisPlatsen');
-    Route::post('/zoeken/googleMaps/gids', 'ApiZoekenController@googleMapsZoekenGids');
-
-    Route::get('/get/wedstrijden/{year}/{month}', 'ApiCalenderController@index');
-
-    route::post('/save/visVangst', 'VisvangsController@opslaanVanVisVangst');
-
-    Route::post('/get/pieChart', 'ApiAdminMainController@pieChart');
-    Route::post('/get/alleVissen', 'ApiAdminMainController@allGavnagenVis');
-    Route::post('/get/visWeer', 'ApiAdminMainController@visWeer');
-
-    Route::get('/get/wheater/{location?}', 'ApiWeaterControllr@index');
-
-    Route::post('/zoeken/alles', 'ApiZoekenController@alles');
-
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
+Route::post('/add/content','AddContentController@add');
+Route::get('/add/content',function(){
+    return 'okey';
+});
+
+Route::get('/get/markers/{type}','ApiGoogleMaps@markers');
+Route::get('/get/marker/{id}','ApiGoogleMaps@marker');
+Route::post('/zoeken/googleMaps','ApiZoekenController@googleMapsZoeken');
+Route::post('/zoeken/googleMaps/visPlatsen','ApiZoekenController@googleMapsZoekenVisPlatsen');
+Route::post('/zoeken/googleMaps/gids','ApiZoekenController@googleMapsZoekenGids');
+
+Route::get('/get/wedstrijden/{year}/{month}','ApiCalenderController@index');
+
+route::post('/save/visVangst','VisvangsController@opslaanVanVisVangst');
+
+Route::post('/get/pieChart','ApiAdminMainController@pieChart');
+Route::post('/get/alleVissen','ApiAdminMainController@allGavnagenVis');
+Route::post('/get/visWeer','ApiAdminMainController@visWeer');
+
+Route::get('/get/wheater/{location?}','ApiWeaterControllr@index');
+
+Route::post('/zoeken/alles','ApiZoekenController@alles');
