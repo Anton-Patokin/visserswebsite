97c6e77df3aa7cd199917eb344c118ef1f6650a4
Anton-Patokin <patokin.anton@gmail.com>
2017-06-10 23:30:57 +0200

    server file upload backslashed bug

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -10,13 +10,13 @@ class FileUploadController extends Controller
     public function fileUpload($file)
     {
         $fileName = date("Ymdsmd") . $file->getClientOriginalName();
-        $destinationPath = 'uploads\\';
+        $destinationPath = 'uploads/';
 
         if ($file->move($destinationPath, $fileName)) {
             $max_with = 500;
             $max_height = 300;
-            Image::make(public_path($destinationPath . $fileName))->resize($max_with, $max_height)->save($destinationPath . 'thumbnail\\' . $fileName);
-            Image::make(public_path($destinationPath . $fileName))->resize($max_with * 2, $max_height * 2)->save($destinationPath . 'big\\' . $fileName);
+            Image::make(public_path($destinationPath . $fileName))->resize($max_with, $max_height)->save($destinationPath . 'thumbnail/' . $fileName);
+            Image::make(public_path($destinationPath . $fileName))->resize($max_with * 2, $max_height * 2)->save($destinationPath . 'big/' . $fileName);
             return $fileName;
         }
     }
