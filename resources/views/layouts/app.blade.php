ac6a72148842a0fadd54d2a3cdf6b0f96c7799c6
Anton-Patokin <patokin.anton@gmail.com>
2017-06-11 18:17:48 +0200

    google tag manager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,11 @@
     <title>{{ config('app.name', 'Fishing Friends') }}</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('head')
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-P6KXFVW');</script>
 </head>
 <body ng-app="myApp" ng-controller="MainController" class="background-gray">
 @include('layouts.top')
@@ -58,6 +63,14 @@
 </script>
 
 @yield('script')
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+    ga('create', 'UA-100835581-1', 'auto');
+    ga('send', 'pageview');
+</script>
 </body>
 </html>
