edbfea62deb962c73253cbef7a1e27d2c2887966
Anton-Patokin <patokin.anton@gmail.com>
2017-06-11 15:59:44 +0200

    htacces caches

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,23 +35,23 @@
 @endif
 <script type="text/javascript" src="{{url('/js/libs/angular.min.js')}}"></script>
 <script type="text/javascript" src="{{url('/js/libs/lodash.min.js')}}"></script>
-<script src="{{url('/js/libs/angular-simple-logger.min.js')}}"></script>
-<script src="{{url('/js/libs/angular-google-maps.js')}}"></script>
-<script src="{{url('/js/libs/angular-messages.js')}}"></script>
-<script src="{{url('/js/libs/angular-animate.min.js')}}"></script>
-<script src="{{url('/js/libs/angular-aria.min.js')}}"></script>
-<script src="{{url('/js/libs/angular-material.min.js')}}"></script>
-<script src="{{ asset('js/base.js') }}"></script>
+<script type="text/javascript" src="{{url('/js/libs/angular-simple-logger.min.js')}}"></script>
+<script type="text/javascript" src="{{url('/js/libs/angular-google-maps.js')}}"></script>
+<script type="text/javascript" src="{{url('/js/libs/angular-messages.js')}}"></script>
+<script type="text/javascript" src="{{url('/js/libs/angular-animate.min.js')}}"></script>
+<script type="text/javascript" src="{{url('/js/libs/angular-aria.min.js')}}"></script>
+<script type="text/javascript" src="{{url('/js/libs/angular-material.min.js')}}"></script>
+<script type="text/javascript" src="{{ asset('js/base.js') }}"></script>
 <script type="text/javascript" src="{{url('/js/libs/angular-cookies.min.js')}}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/angular-google-chart/1.0.0-beta.1/ng-google-chart.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/angular-google-chart/1.0.0-beta.1/ng-google-chart.min.js"></script>
 
-<script src="{{ asset('js/app.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 <script id="searchbox.tpl.html" type="text/ng-template">
     <input class="form-search input-sm" type="text" placeholder="Locatie" z-index="50000">
 </script>
 <link href="{{ asset('css/angular-material.min.css') }}" rel="stylesheet">
 
-<script async>
+<script async type="text/javascript">
     $(document).ready(function(){function o(o){$("#vertikalscroll").animate({scrollTop:t},2e3,function(){i=!1})}if($(document).scroll(function(o){window.scrollY>=400?$("#go_top").fadeIn():$("#go_top").fadeOut()}),$("#go_top").click(function(){$("html, body").animate({scrollTop:0},"slow")}),$('[data-toggle="tooltip"]').tooltip(),$("#vertikalscroll").length){console.lo;var t=0,l=$("#vertikalscroll")[0].scrollHeight-300,i=!1;$("#go-to-top").hide();var n=!0;l<400?($("#go-to-bottom").hide(),$("#go-to-top").hide()):setInterval(function(){n?i||(i=!0,t+=300,$("#go-to-top").show(),t>l&&(t=l,t=l-300,$("#go-to-bottom").hide(),n=!1),o(t)):i||(i=!0,t-=300,$("#go-to-bottom").show(),t<0&&(t=0,n=!0,$("#go-to-top").hide()),o(t))},5e3),$("#go-to-bottom").on("click",function(){i||(i=!0,t+=300,$("#go-to-top").show(),t>l&&(t=l,t=l-300,$("#go-to-bottom").hide()),o(t))}),$("#go-to-top").on("click",function(){i||(i=!0,t-=300,$("#go-to-bottom").show(),t<0&&(t=0,$("#go-to-top").hide()),o(t))}),$(".clearValue").on("click",function(){t=0})}});
 </script>
 
