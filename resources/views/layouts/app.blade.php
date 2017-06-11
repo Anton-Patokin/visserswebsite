9a10b97c478403037a3764696efa4bad8583df6a
Anton-Patokin <patokin.anton@gmail.com>
2017-06-11 18:19:40 +0200

    remove google tag manager ad analytiks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,7 @@
     <title>{{ config('app.name', 'Fishing Friends') }}</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('head')
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-P6KXFVW');</script>
+    
 </head>
 <body ng-app="myApp" ng-controller="MainController" class="background-gray">
 @include('layouts.top')
