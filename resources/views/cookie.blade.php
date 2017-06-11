a65d36e7c0e1acc1faef701f80c3bf3121dc6a16
Anton-Patokin <patokin.anton@gmail.com>
2017-06-11 20:00:29 +0200

    coocie and search bug

--- resources/views/cookie.blade.php
+++ resources/views/cookie.blade.php
@@ -1,23 +1,4 @@
-<style>
-    .cookie {
-        position: fixed;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        z-index: 200;
-        background-color: #323b44;
-        color: #fff;
-        height: 75px;
-    }
 
-    .cookie .content {
-        padding-top: 22px;
-    }
-
-    .cookie .content p {
-        padding-top: 8px;
-    }
-</style>
 
 <div ng-controller="coockieSetController">
     <div class="cookie" ng-if="show_cookie_accesp">
