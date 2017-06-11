b687a9b0bfca5e3084c7c57e4df9ec14a2b0727c
Anton-Patokin <patokin.anton@gmail.com>
2017-06-11 11:18:57 +0200

    change glopal path for cors bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
 {{--<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-animate.min.js"></script>--}}
 {{--<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDJBGY58S0ptq6KlFxYIpNLTIEW8mBKhk4&libraries=places&sensor=false"></script>--}}
 {{--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDJBGY58S0ptq6KlFxYIpNLTIEW8mBKhk4&libraries=places" async defer></script>--}}
-<script>var ROUTEFRONT = 'https://www.fishingfriends.be';</script>
+<script>var ROUTEFRONT = '';</script>
 <script type="text/javascript" src="{{url('/js/libs/angular.min.js')}}"></script>
 
 <script type="text/javascript" src="{{url('/js/libs/lodash.min.js')}}"></script>
