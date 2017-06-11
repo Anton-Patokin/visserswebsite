c85a525459fe1dcc261442e81655896f8fc92348
Anton-Patokin <patokin.anton@gmail.com>
2017-06-11 15:27:34 +0200

    remove async

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,17 +7,9 @@
     <meta name="viewport"
           content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi">
     <link rel="shortcut icon" href="{{url('/images/icon/favicon.png')}}" type="image/x-icon">
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <!-- Styles -->
+    <title>{{ config('app.name', 'Fishing Friends') }}</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    {{--<!-- Scripts -->--}}
-    {{--<script>--}}
-    {{--window.Laravel = {!! json_encode([--}}
-    {{--'csrfToken' => csrf_token(),--}}
-    {{--]) !!};--}}
-    {{--</script>--}}
     @yield('head')
 </head>
 <body ng-app="myApp" ng-controller="MainController" class="background-gray">
@@ -30,10 +22,9 @@
 </div>
 <button id="go_top" class="btn btn-default button-up"><i class="glyphicon glyphicon-menu-up"></i></button>
 @if(Auth::user())
-    @include('actions.vis-vangst')
+@include('actions.vis-vangst')
 @endif
 @include('components.footer')
-
 @if(url('/')== 'https://fishingfriends.be/')
     <script>var ROUTEFRONT = '';</script>
 
@@ -42,24 +33,16 @@
 
 @endif
 <script type="text/javascript" src="{{url('/js/libs/angular.min.js')}}"></script>
-<script async type="text/javascript" src="{{url('/js/libs/lodash.min.js')}}"></script>
-<script async src="{{url('/js/libs/angular-simple-logger.min.js')}}"></script>
-<script async src="{{url('/js/libs/angular-google-maps.js')}}"></script>
-<script async src="{{url('/js/libs/angular-messages.js')}}"></script>
-<script async src="{{url('/js/libs/angular-animate.min.js')}}"></script>
-<script async src="{{url('/js/libs/angular-aria.min.js')}}"></script>
-{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.5/angular-messages.min.js"></script>--}}
-<script async src="{{url('/js/libs/angular-material.min.js')}}"></script>
-{{--<script src="{{url('/js/libs/ngAutocomplete.js')}}"></script>--}}
-<script async src="{{ asset('js/base.js') }}"></script>
-{{--<script src="{{url('/js/file-reader.js')}}"></script>--}}
-{{--<script src="{{url('/js/MainGoogleMaps.js')}}"></script>--}}
-{{--<script src="{{asset('js/vissersActiviteiten.js')}}"></script>--}}
-{{--<script src="{{asset('js/GenericChartCtrl.js')}}"></script>--}}
-{{--<script src="{{url('/js/smallGoogleMaps.js')}}"></script>--}}
-<script async type="text/javascript" src="{{url('/js/libs/angular-cookies.min.js')}}"></script>
-<script async src="https://cdnjs.cloudflare.com/ajax/libs/angular-google-chart/1.0.0-beta.1/ng-google-chart.min.js"
-        type="text/javascript"></script>
+<script type="text/javascript" src="{{url('/js/libs/lodash.min.js')}}"></script>
+<script src="{{url('/js/libs/angular-simple-logger.min.js')}}"></script>
+<script src="{{url('/js/libs/angular-google-maps.js')}}"></script>
+<script src="{{url('/js/libs/angular-messages.js')}}"></script>
+<script src="{{url('/js/libs/angular-animate.min.js')}}"></script>
+<script src="{{url('/js/libs/angular-aria.min.js')}}"></script>
+<script src="{{url('/js/libs/angular-material.min.js')}}"></script>
+<script src="{{ asset('js/base.js') }}"></script>
+<script type="text/javascript" src="{{url('/js/libs/angular-cookies.min.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/angular-google-chart/1.0.0-beta.1/ng-google-chart.min.js" type="text/javascript"></script>
 
 <script src="{{ asset('js/app.js') }}"></script>
 <script id="searchbox.tpl.html" type="text/ng-template">
@@ -67,112 +50,8 @@
 </script>
 <link href="{{ asset('css/angular-material.min.css') }}" rel="stylesheet">
 
-<script>
-    $(document).ready(function () {
-
-        $(document).scroll(function ($this) {
-
-            var y = window.scrollY;
-            if (y >= 400) {
-                $('#go_top').fadeIn();
-            } else {
-                $('#go_top').fadeOut();
-            }
-
-
-        })
-        $('#go_top').click(function () {
-            $("html, body").animate({scrollTop: 0}, "slow");
-        })
-
-        $('[data-toggle="tooltip"]').tooltip();
-
-        if ($("#vertikalscroll").length) {
-            console.lo
-            var scrolled = 0;
-            var maxScroll = $("#vertikalscroll")[0].scrollHeight - 300;
-            var move = false;
-            var moveSize = 300;
-            $("#go-to-top").hide();
-            var moveDown = true;
-
-
-            if (maxScroll < 400) {
-                $("#go-to-bottom").hide();
-                $("#go-to-top").hide();
-            } else {
-                setInterval(function () {
-                    if (moveDown) {
-                        if (!move) {
-                            move = true;
-                            scrolled = scrolled + moveSize;
-                            $("#go-to-top").show();
-                            if (scrolled > maxScroll) {
-                                scrolled = maxScroll;
-                                scrolled = maxScroll - moveSize;
-                                $("#go-to-bottom").hide();
-                                moveDown = false;
-                            }
-                            scrolY(scrolled)
-                        }
-                    } else {
-                        if (!move) {
-                            move = true;
-                            scrolled = scrolled - moveSize;
-                            $("#go-to-bottom").show();
-
-                            if (scrolled < 0) {
-                                scrolled = 0
-                                moveDown = true;
-                                $("#go-to-top").hide();
-                            }
-                            scrolY(scrolled)
-                        }
-                    }
-
-                }, 5000);
-            }
-
-            $("#go-to-bottom").on("click", function () {
-                if (!move) {
-                    move = true;
-                    scrolled = scrolled + moveSize;
-                    $("#go-to-top").show();
-                    if (scrolled > maxScroll) {
-                        scrolled = maxScroll;
-                        scrolled = maxScroll - moveSize;
-                        $("#go-to-bottom").hide();
-
-                    }
-                    scrolY(scrolled)
-                }
-            });
-            $("#go-to-top").on("click", function () {
-                if (!move) {
-                    move = true;
-                    scrolled = scrolled - moveSize;
-                    $("#go-to-bottom").show();
-
-                    if (scrolled < 0) {
-                        scrolled = 0
-                        $("#go-to-top").hide();
-                    }
-                    scrolY(scrolled)
-                }
-            });
-            function scrolY(scroll) {
-                $("#vertikalscroll").animate({
-                    scrollTop: scrolled
-                }, 2000, function () {
-                    move = false;
-                });
-            }
-
-            $(".clearValue").on("click", function () {
-                scrolled = 0;
-            });
-        }
-    });
+<script async>
+    $(document).ready(function(){function o(o){$("#vertikalscroll").animate({scrollTop:t},2e3,function(){i=!1})}if($(document).scroll(function(o){window.scrollY>=400?$("#go_top").fadeIn():$("#go_top").fadeOut()}),$("#go_top").click(function(){$("html, body").animate({scrollTop:0},"slow")}),$('[data-toggle="tooltip"]').tooltip(),$("#vertikalscroll").length){console.lo;var t=0,l=$("#vertikalscroll")[0].scrollHeight-300,i=!1;$("#go-to-top").hide();var n=!0;l<400?($("#go-to-bottom").hide(),$("#go-to-top").hide()):setInterval(function(){n?i||(i=!0,t+=300,$("#go-to-top").show(),t>l&&(t=l,t=l-300,$("#go-to-bottom").hide(),n=!1),o(t)):i||(i=!0,t-=300,$("#go-to-bottom").show(),t<0&&(t=0,n=!0,$("#go-to-top").hide()),o(t))},5e3),$("#go-to-bottom").on("click",function(){i||(i=!0,t+=300,$("#go-to-top").show(),t>l&&(t=l,t=l-300,$("#go-to-bottom").hide()),o(t))}),$("#go-to-top").on("click",function(){i||(i=!0,t-=300,$("#go-to-bottom").show(),t<0&&(t=0,$("#go-to-top").hide()),o(t))}),$(".clearValue").on("click",function(){t=0})}});
 </script>
 
 @yield('script')
